2b3df3b7ca9310010640e34c42b616cb661b146f
Theorhd <theo.richard8654@gmail.com>
2024-03-06 14:34:28 +0100

    Ajout Header

---
+++ app/public/wp-content/themes/nathalieMota/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header id="masthead" class="site-header" role="banner">
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+
+        <div class="site-branding">
+            <?php
+            if ( is_front_page() && is_home() ) : ?>
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <?php else : ?>
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+            <?php
+            endif;
+
+            $description = get_bloginfo( 'description', 'display' );
+            if ( $description || is_customize_preview() ) : ?>
+                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+            <?php
+            endif; ?>
+        </div><!-- .site-branding -->
+
+        <?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
+        
+        </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->
+
+    <div id="content" class="site-content">
