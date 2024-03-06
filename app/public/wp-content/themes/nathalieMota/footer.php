d17dd3ef7ae4ccf8d2fec594fa00377adfbe30c0
Theorhd <theo.richard8654@gmail.com>
2024-03-06 14:34:57 +0100

    Ajout Footer

---
+++ app/public/wp-content/themes/nathalieMota/footer.php
@@ -0,0 +1,9 @@
+
+<?php get_template_part( 'template-parts/contact' ); ?>
+
+</div><!-- #content -->
+
+<footer id="footer" class="site-footer">
+        <?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+        
+</footer>
