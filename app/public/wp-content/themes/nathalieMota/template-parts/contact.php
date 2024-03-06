11f61019801445ae31d8231b039ed71e63a11ad3
Theorhd <theo.richard8654@gmail.com>
2024-03-06 14:35:13 +0100

    Ajout modale Contact

---
+++ app/public/wp-content/themes/nathalieMota/template-parts/contact.php
@@ -0,0 +1,10 @@
+<div class="contact-overlay">
+    <div class="popup-contact">
+        <div class="popup-header">
+            <h2>CONTACT</h2>
+        </div>
+        <div class="popup-form">
+            <?php echo do_shortcode('[contact-form-7 id="b2086f3" title="Contact Form"]'); ?>
+        </div>
+    </div>
+</div>
