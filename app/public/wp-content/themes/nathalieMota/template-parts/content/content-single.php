c9d222963a852df62e8c9107491b121e0751372d
Theorhd <theo.richard8654@gmail.com>
2024-03-08 04:40:20 +0100

    Ajout Responsive Single Post

--- app/public/wp-content/themes/nathalieMota/template-parts/content/content-single.php
+++ app/public/wp-content/themes/nathalieMota/template-parts/content/content-single.php
@@ -20,57 +20,26 @@ $format = get_field('format');
 $type = get_field('type');
 ?>
 
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="entry-content">
-        <?php if ( $format === 'paysage') : { ?>
-            <div class="content-container">
-                <div class="left-side-paysage">
-                    <h2><?php echo $titre; ?></h2>
-                    <p>Référence : <?php echo $refphoto; ?></p>
-                    <p>Catégorie : <?php echo $categorie[0]; ?></p>
-                    <p>Format : <?php echo $format; ?></p>
-                    <p>Type : <?php echo $type; ?></p>
-                    <p>Année : <?php echo $annee; ?></p>
-                    <div class="ctaphoto-contact">
-                        <h5>Cette photo vous intéresse ?</h5>
-                        <button>Contact</button>
+        <?php if ( wp_is_mobile() === false ) : { ?>
+            <?php if ( $format === 'paysage') : { ?>
+                <div class="content-container">
+                    <div class="left-side-paysage">
+                        <h2><?php echo $titre; ?></h2>
+                        <p>Référence : <?php echo $refphoto; ?></p>
+                        <p>Catégorie : <?php echo $categorie[0]; ?></p>
+                        <p>Format : <?php echo $format; ?></p>
+                        <p>Type : <?php echo $type; ?></p>
+                        <p>Année : <?php echo $annee; ?></p>
+                        <div class="ctaphoto-contact">
+                            <h5>Cette photo vous intéresse ?</h5>
+                            <button>Contact</button>
+                        </div>
+                    </div>
+                    <div class="right-side-paysage" style="background-image: url('<?php echo $image['url']; ?>')">
                     </div>
                 </div>
-                <div class="right-side-paysage">
-                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-                </div>
-            </div>
-            <div class="alsoLike-container">
-                <h3>Vous aimerez aussi</h3>
-                <div class="alsoLike-photos">
-                    <div class="alsoLike-photo1"></div>
-                    <div class="alsoLike-photo2"></div>
-                    <div class="alsoLike-photo3"></div>
-                </div>
-            </div>
-        </div>
-        <?php } endif; ?>
-
-
-        <?php if ( $format === 'portrait') : { ?> 
-            <div class="content-container">
-                <div class="left-side-portrait">
-                    <h2><?php echo $titre; ?></h2>
-                    <p>Référence : <?php echo $refphoto; ?></p>
-                    <p>Catégorie : <?php echo $categorie[0]; ?></p>
-                    <p>Format : <?php echo $format; ?></p>
-                    <p>Type : <?php echo $type; ?></p>
-                    <p>Année : <?php echo $annee; ?></p>
-                    <div class="ctaphoto-contact">
-                        <h5>Cette photo vous intéresse ?</h5>
-                        <button>Contact</button>
-                    </div>
-                </div>
-                <div class="right-side-portrait">
-                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-                </div>
-            </div>
                 <div class="alsoLike-container">
                     <h3>Vous aimerez aussi</h3>
                     <div class="alsoLike-photos">
@@ -79,8 +48,98 @@ $type = get_field('type');
                         <div class="alsoLike-photo3"></div>
                     </div>
                 </div>
+            </div>
+            <?php } endif; ?>
         <?php } endif; ?>
 
+        <?php if ( wp_is_mobile() === false ) : { ?>
+            <?php if ( $format === 'portrait') : { ?> 
+                <div class="content-container">
+                    <div class="left-side-portrait">
+                        <h2><?php echo $titre; ?></h2>
+                        <p>Référence : <?php echo $refphoto; ?></p>
+                        <p>Catégorie : <?php echo $categorie[0]; ?></p>
+                        <p>Format : <?php echo $format; ?></p>
+                        <p>Type : <?php echo $type; ?></p>
+                        <p>Année : <?php echo $annee; ?></p>
+                        <div class="ctaphoto-contact">
+                            <h5>Cette photo vous intéresse ?</h5>
+                            <button>Contact</button>
+                        </div>
+                    </div>
+                    <div class="right-side-paysage" style="background-image: url('<?php echo $image['url']; ?>')">
+                    </div>
+                </div>
+                    <div class="alsoLike-container">
+                        <h3>Vous aimerez aussi</h3>
+                        <div class="alsoLike-photos">
+                            <div class="alsoLike-photo1"></div>
+                            <div class="alsoLike-photo2"></div>
+                            <div class="alsoLike-photo3"></div>
+                        </div>
+                    </div>
+            <?php } endif; ?>
+        <?php } endif; ?>
+
+        <?php if ( wp_is_mobile() === true ) : { ?>
+            <?php if ( $format === 'paysage') : { ?>
+                <div class="content-container">
+                    <div class="right-side-paysage" style="background-image: url('<?php echo $image['url']; ?>')">
+                    </div>
+                    <div class="left-side-paysage">
+                        <h2><?php echo $titre; ?></h2>
+                        <p>Référence : <?php echo $refphoto; ?></p>
+                        <p>Catégorie : <?php echo $categorie[0]; ?></p>
+                        <p>Format : <?php echo $format; ?></p>
+                        <p>Type : <?php echo $type; ?></p>
+                        <p>Année : <?php echo $annee; ?></p>
+                        <div class="ctaphoto-contact">
+                            <h5>Cette photo vous intéresse ?</h5>
+                            <button>Contact</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="alsoLike-container">
+                    <h3>Vous aimerez aussi</h3>
+                    <div class="alsoLike-photos">
+                        <div class="alsoLike-photo1"></div>
+                        <div class="alsoLike-photo2"></div>
+                        <div class="alsoLike-photo3"></div>
+                    </div>
+                </div>
+            </div>
+            <?php } endif; ?>
+        <?php } endif; ?>
+
+        <?php if ( wp_is_mobile() === true ) : { ?>
+            <?php if ( $format === 'portrait') : { ?> 
+                <div class="content-container">
+                    <div class="right-side-portrait" style="background-image: url('<?php echo $image['url']; ?>')">
+                    </div>
+                    <div class="left-side-portrait">
+                        <h2><?php echo $titre; ?></h2>
+                        <p>Référence : <?php echo $refphoto; ?></p>
+                        <p>Catégorie : <?php echo $categorie[0]; ?></p>
+                        <p>Format : <?php echo $format; ?></p>
+                        <p>Type : <?php echo $type; ?></p>
+                        <p>Année : <?php echo $annee; ?></p>
+                        <div class="ctaphoto-contact">
+                            <h5>Cette photo vous intéresse ?</h5>
+                            <button>Contact</button>
+                        </div>
+                    </div>
+                </div>
+                    <div class="alsoLike-container">
+                        <h3>Vous aimerez aussi</h3>
+                        <div class="alsoLike-photos">
+                            <div class="alsoLike-photo1"></div>
+                            <div class="alsoLike-photo2"></div>
+                            <div class="alsoLike-photo3"></div>
+                        </div>
+                    </div>
+            <?php } endif; ?>
+        <?php } endif; ?>
+        
         <?php
         wp_link_pages( array(
             'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'your-theme-text-domain' ),
